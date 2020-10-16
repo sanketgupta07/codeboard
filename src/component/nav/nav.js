54e6a30a4521302c5a6e0fdf8b9153ca2d340f89
Sanket Gupta <sanketgupta07@gmail.com>
2020-10-16 14:37:59 +0530

    deploy yml update

--- src/component/nav/nav.js
+++ src/component/nav/nav.js
@@ -7,7 +7,7 @@ export default function AppNav() {
   return (
     <>
       <Navbar bg="dark" variant="dark">
-        <Navbar.Brand as={Link} to="/">
+        <Navbar.Brand as={Link} to="/codeboard">
           <RiDashboardFill />
         </Navbar.Brand>
         <Nav className="mr-auto">
