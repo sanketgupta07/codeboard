55d722b0a2cab99d96e696e8157f360b708652c1
Sanket Gupta <sanketgupta07@gmail.com>
2020-12-11 02:36:04 +0530

    update

--- src/component/about/aboutus.js
+++ src/component/about/aboutus.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { Card, Jumbotron } from "react-bootstrap";
-import { RiBugFill } from "react-icons/ri";
+import { RiBugFill, RiTwitterFill } from "react-icons/ri";
 import { Redirect } from "react-router";
 import { AuthContext } from "../../router";
 
@@ -11,7 +11,10 @@ export default function AboutUs(params) {
   }
   return (
     <Jumbotron fluid style={{ background: "none" }}>
-      <h3>Thanks for visiting codeboard</h3>
+      <h3>
+        Thanks for visiting codeboard. This app is only built for learning
+        purpose.
+      </h3>
       <p>
         <RiBugFill />
         &nbsp;
@@ -20,6 +23,13 @@ export default function AboutUs(params) {
           target="_blank"
         >
           To Report a bug/issue{" "}
+        </Card.Link>
+      </p>
+      <p>
+        You may contact or follow me on twitter <RiTwitterFill />
+        &nbsp;
+        <Card.Link href="https://twitter.com/sanketgupta007" target="_blank">
+          @sanketgupta007{" "}
         </Card.Link>
       </p>
     </Jumbotron>
